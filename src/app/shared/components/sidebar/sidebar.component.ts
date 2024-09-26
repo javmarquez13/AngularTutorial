69c50954924f1cc646dd8c6aa27558b726d4026a
Javier Marquez <javier_marquez@jabil.com>
2024-09-26 15:38:57 -0600

    Adding icons arrays and logic into constructor

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { isNgContainer } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: 
+  [
+    CommonModule
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  public mainMenu: {
+    defaultOptions: Array<any>;
+    accessLink: Array<any>;
+  } = { defaultOptions: [], accessLink: [] };
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil-estate',
+        route: '\home'
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil-search',
+        route: '\buscar'
+      }
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'ola',
+        icon: 'uil-pathfinder'
+      }
+    ]
+  }
 }
